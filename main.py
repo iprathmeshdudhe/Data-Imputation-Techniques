2a733a3ce343d3f1ad3379177d408cf917b8bdcf
iprathmeshdudhe <40405020+iprathmeshdudhe@users.noreply.github.com>
2024-03-21 18:55:30 +0100

    modified according to SSL and dataset_config file

--- main.py
+++ main.py
@@ -10,9 +10,11 @@ from utils.plot_predictions import plot_line
 from imputers.knn import KNN as knn
 from imputers.mice import MiceImputer as mice
 from imputers.nnimputer import NeuralNetworkImputer
+from imputers.ssl_imputer import SSLImputer
 from imputers.simple_imputer import SimpleImpute as s_impute
 
 from evals.eval import Evaluation as eval
+from dataset_config import MimicConfig, TudConfig, HopperConfig
 
 
 import warnings
@@ -32,33 +34,39 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     if args.impute:
-        imputer, dataset_name, observation_size, missing_cols, missing_type = get_config("impute")
+        imputer, dataset_name, _, _, _ = get_config("impute")
 
         if dataset_name == "mimiciv":
-            columns = ["vent_etco2", "vent_fio2", "vital_spo2", "vital_hr","vent_rrtot", "blood_paco2", "blood_pao2"]
-        else:
-            columns = [i for i in range(observation_size)]
+            dataset_cfg = MimicConfig()
+            #columns = ["vent_etco2", "vent_fio2", "vital_spo2", "vital_hr","vent_rrtot", "blood_paco2", "blood_pao2"]
+        elif dataset_name == "tud":
+            dataset_cfg = TudConfig()
+            #columns = [i for i in range(observation_size)]
+        elif dataset_name == "hopper":
+            dataset_cfg = HopperConfig()
 
+        columns = dataset_cfg.state_vector
         print("Dataset: ", dataset_name.capitalize(), " Imputer: ", imputer)
-        dataframe = load_obs_data(dataset_name, columns, missing_type)
+        dataframe = load_obs_data(dataset_cfg)
 
         imputers = {
         "ffill" : s_impute.fwd_impute,
         "knn" : knn.impute_data,
         "mice" : mice.mice_impute,
         "miceforest" : mice.mice_forest,
-        "nn" : None
+        "nn" : None,
+        "ssl" : None
         }
         
         if imputer in imputers.keys():
             if imputer == "nn":
-                nni = NeuralNetworkImputer(dataset_name, columns, missing_type, missing_cols)
+                nni = NeuralNetworkImputer(dataset_cfg)
 
-                miss_data_path, train_run, test_run, train_test, model_path = get_yaml()
+                train_run, test_run, train_test, model_path = get_yaml()
 
-                missing_data = pd.read_csv(miss_data_path, usecols=columns)
+                missing_data = pd.read_csv(dataset_cfg.missing_data_path, usecols=dataset_cfg.state_vector)
                 
-                for col in missing_cols:
+                for col in dataset_cfg.missing_state_vector:
                     
                     if train_run:
                         nni.fit(col)
@@ -72,33 +80,53 @@ if __name__ == "__main__":
                 if train_run:
                     pass
                 else:
-                    nni.save_imputed_data(missing_data)
+                    nni.save_imputed_data(dataset_cfg.imputed_data_path, missing_data)
+
+            elif imputer == "ssl":
+                ssl = SSLImputer(dataset_cfg)
+                ssl.build_model()
+                ssl.fit(dataframe)
+
+                # TODO: Add Intermediate step to fill the missing value for prediction
+                ssl.transform(dataframe)
+
                                
             else:
-                imputers[imputer](dataframe, dataset_name, missing_type)
+                imputers[imputer](dataframe, dataset_cfg)
 
     elif args.create_missing:
 
-        dataset_name, dataset_path, observation_size, action_size, missing_cols, missing_type, p_missing, steps = get_config("create_missing")
+        dataset_name, _, _, _, _, missing_type, p_missing, steps = get_config("create_missing")
         print("Dataset: ", dataset_name.capitalize())
 
         if dataset_name == "mimiciv":
-            columns = ["vent_etco2", "vent_fio2", "vital_spo2", "vital_hr","vent_rrtot", "blood_paco2", "blood_pao2"]
-        else:
-            columns = [i for i in range(observation_size)]
+            dataset_cfg = MimicConfig()
+        elif dataset_name == "tud":
+            dataset_cfg = TudConfig()
+        elif dataset_name == "hopper":
+            dataset_cfg = HopperConfig()
+            
+        columns = dataset_cfg.state_vector
 
-        dataframe = load_full_data(dataset_path, columns)
+        dataframe = load_full_data(dataset_cfg.full_data_path, columns)
 
-        create_missing_dataset(dataset_name, dataframe, observation_size, missing_cols, steps=None, random=True, percent = p_missing)
+        create_missing_dataset(dataset_cfg.data_name, dataframe, dataset_cfg.missing_state_vector, steps=None, random=True, percent = p_missing)
 
     elif args.eval:
-        dataset_name, observation_size, missing_cols, imputer, imputed_data_path = get_config("eval")
+        dataset_name, _, _, imputer, _ = get_config("eval")
         #og_col_1, og_col_2, imputed_col_1, imputed_col_2 = load_eval_columns(dataset_name, observation_size, imputed_data_path)
-        eval_cols_dict = load_eval_columns(dataset_name, missing_cols, imputed_data_path)
-
         
-        for i, col in enumerate(missing_cols, start=1):
+
+        if dataset_name == "mimiciv":
+            dataset_cfg = MimicConfig()
+        elif dataset_name == "tud":
+            dataset_cfg = TudConfig()
+        elif dataset_name == "hopper":
+            dataset_cfg = HopperConfig()
+
+        eval_cols_dict = load_eval_columns(dataset_cfg, imputer)
+        for i, col in enumerate(dataset_cfg.missing_state_vector, start=1):
             og_col = eval_cols_dict[f"og_{col}_{i}"]
             imputed_col = eval_cols_dict[f"imputed_{col}_{i}"]
-            eval.scores(og_col, imputed_col, imputed_data_path, col)
+            eval.scores(og_col, imputed_col, dataset_cfg.imputed_data_path, imputer, col)
             plot_line(imputed_col[:200], og_col[:200], dataset_name, imputer, col)

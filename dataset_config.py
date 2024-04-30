3a752ee5995ad1e3325218bd4987b6c5320b9c96
Prathmesh Dudhe <prathmeshdudhe15@gmail.com>
2024-04-30 17:00:17 +0200

    added json as a imputation config

--- dataset_config.py
+++ dataset_config.py
@@ -1,15 +1,23 @@
+import json
 class MIMICIV_config:
     def __init__(self):
-        self.data_name = 'MIMICIV'
-        self.full_data_path = 'dataset/mimiciv_full_data.csv'
-        self.imputed_data_path = 'dataset/mimiciv_blood_chlorid_imp'        # Yes, it is correct, don't add .csv extension
-        self.missing_data_path = 'dataset/mimiciv_blood_chlorid_w_missing_values_random.csv'
+
+        f = open('imp_config.json')
+        data_info = json.load(f)
+
+        self.data_name = data_info["name"]
+        self.full_data_path = data_info["full_data_path"]
+        self.imputed_data_path = data_info["imputed_data_path"]
+        self.missing_data_path = data_info["missing_data_path"]
 
 
         # These are only useful for GRU (nn) imputation and evaluation
-        '''
+        
         # Indepedent variables
-        self.state_vector = []'''
+        self.state_vector = data_info["state_vector"]
 
         # Target variable
-        self.missing_state_vector = ["blood_chlorid"]
+        self.missing_state_vector = data_info["missing_state_vector"]
+
+
+        
